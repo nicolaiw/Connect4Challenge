5868e1f805c95827d12725fe4541ae39718fbfab
Jallah <puncher@gmx.net>
2015-07-08 20:50:05 +0200

    add conditional compiling due to not supported stuff on MONO

--- src/Connect4Challenge/Connect4ChallengeBootstrapper.fs
+++ src/Connect4Challenge/Connect4ChallengeBootstrapper.fs
@@ -10,79 +10,72 @@ open System.Security.Policy
 open Connect4Challenge.Interface
 open System
 
-
 // http://fsharpforfunandprofit.com/posts/cli-types/
-let tryCast<'a> o = match box o with
-                        | :? 'a as output -> Some output
-                        | _ -> None
+let tryCast<'a> o = 
+    match box o with
+    | :? 'a as output -> Some output
+    | _ -> None
 
 //NOT USED
-let getInterfaceFromAssembly<'a> assemblyPath =
-    let assembly = Assembly.LoadFrom(assemblyPath);
-    let interfaceType = assembly.GetTypes()
-                        |> Seq.tryFind (fun a -> a.GetInterface(typeof<'a>.Name) <> null)
+let getInterfaceFromAssembly<'a> assemblyPath = 
+    let assembly = Assembly.LoadFrom(assemblyPath)
+    let interfaceType = assembly.GetTypes() |> Seq.tryFind (fun a -> a.GetInterface(typeof<'a>.Name) <> null)
     match interfaceType with
-    | Some(ifType) -> match System.Activator.CreateInstance(ifType) |> tryCast<'a> with
-                      | Some(instance) -> instance
-                      | None -> Unchecked.defaultof<'a>
+    | Some(ifType) -> 
+        match System.Activator.CreateInstance(ifType) |> tryCast<'a> with
+        | Some(instance) -> instance
+        | None -> Unchecked.defaultof<'a>
     | None -> Unchecked.defaultof<'a>
-    
 
-//TODO: refactor out the code duplicates in the invoke methods
-type Sandboxer() =
-    
+//MONO does not Support GetHostEvidence. 
+//TODO: if MONO run Code in the full trusted AppDomain. This Application should be started with an restricted user on Linux.
+type Sandboxer() = 
     inherit System.MarshalByRefObject()
-
-    member this.setup(pathToUntrusted) = 
+    
+    member this.setup (pathToUntrusted) = 
         //Setting the AppDomainSetup. It is very important to set the ApplicationBase to a folder 
         //other than the one in which the sandboxer resides.
-        let adSetup = new AppDomainSetup( ApplicationBase = Path.GetFullPath(pathToUntrusted) )
-
+        let adSetup = new AppDomainSetup(ApplicationBase = Path.GetFullPath(pathToUntrusted))
         //Setting the permissions for the AppDomain. We give the permission to execute and to 
         //read/discover the location where the untrusted code is loaded.
         let permSet = new PermissionSet(PermissionState.None)
         permSet.AddPermission(new SecurityPermission(SecurityPermissionFlag.Execution)) |> ignore
-
+#if MONO
+        failwith "mono does not support GetHostEvidence see: http://go-mono.com/status/status.aspx?reference=4.0&profile=4.0&assembly=mscorlib"
+#else
         //We want the sandboxer assembly's strong name, so that we can add it to the full trust list.
         let fullTrustAssembly = typeof<Sandboxer>.Assembly.Evidence.GetHostEvidence<StrongName>()
-
+#endif
         //Now we have everything we need to create the AppDomain, so let's create it.
         let newDomain = AppDomain.CreateDomain("Sandbox", null, adSetup, permSet, fullTrustAssembly)
         //Use CreateInstanceFrom to load an instance of the Sandboxer class into the
         //new AppDomain. 
-        let handle = Activator.CreateInstanceFrom(newDomain, typeof<Sandboxer>.Assembly.ManifestModule.FullyQualifiedName, typeof<Sandboxer>.FullName)
-        
+        let handle = 
+            Activator.CreateInstanceFrom
+                (newDomain, typeof<Sandboxer>.Assembly.ManifestModule.FullyQualifiedName, typeof<Sandboxer>.FullName)
         //Unwrap the new domain instance into a reference in this domain and use it to execute the 
         //untrusted code.
-        let newDomainInstance =  handle.Unwrap() :?> Sandboxer
-
+        let newDomainInstance = handle.Unwrap() :?> Sandboxer
         newDomainInstance
-
-
-     member private this.getInstance<'a>(untrustedAssemblyName: string) =
-        
+    
+    member private this.getInstance<'a> (untrustedAssemblyName : string) = 
         let assembly = Assembly.Load(untrustedAssemblyName)
         let ``type`` = typeof<'a>
-
-        let instanceType = assembly.GetTypes()
-                                |> Seq.tryFind (fun a -> a.IsSubclassOf(``type``))
-        
+        let instanceType = assembly.GetTypes() |> Seq.tryFind (fun a -> a.IsSubclassOf(``type``))
         match instanceType with
-                | Some(ifType) -> match System.Activator.CreateInstance(ifType) |> tryCast<'a> with
-                                  | Some(instance) -> instance
-                                  | None -> Unchecked.defaultof<'a>
-                | None -> Unchecked.defaultof<'a>
+        | Some(ifType) -> 
+            match System.Activator.CreateInstance(ifType) |> tryCast<'a> with
+            | Some(instance) -> instance
+            | None -> Unchecked.defaultof<'a>
+        | None -> Unchecked.defaultof<'a>
     
-    member private this.invoke<'a, 'ret>(untrustedAssemblyName, getMemberValue) =
-        let t = this.getInstance<'a>(untrustedAssemblyName)
-        match tryCast<'ret> (getMemberValue(t)) with
+    member private this.invoke<'a, 'ret> (untrustedAssemblyName, getMemberValue) = 
+        let t = this.getInstance<'a> (untrustedAssemblyName)
+        match tryCast<'ret> (getMemberValue (t)) with
         | Some(T) -> T
         | None -> failwith "Bad return type"
-        
-    member this.invokeProperty<'a, 'ret>(untrustedAssemblyName, memberName) : 'ret =
-        this.invoke(untrustedAssemblyName, fun t-> t.GetType().GetProperty(memberName).GetValue(t, null))
-
-    member this.invokeMethod<'a, 'ret>(untrustedAssemblyName, memberName, [<ParamArray>] args: Object[]) : 'ret =
-        this.invoke(untrustedAssemblyName, fun t -> t.GetType().GetMethod(memberName).Invoke(t, args))
-        
-
+    
+    member this.invokeProperty<'a, 'ret> (untrustedAssemblyName, memberName) : 'ret = 
+        this.invoke (untrustedAssemblyName, fun t -> t.GetType().GetProperty(memberName).GetValue(t, null))
+    member this.invokeMethod<'a, 'ret> (untrustedAssemblyName, memberName, [<ParamArray>] args : Object []) : 'ret = 
+        this.invoke (untrustedAssemblyName, fun t -> t.GetType().GetMethod(memberName).Invoke(t, args))
