a074fdb7c78bb60b0eec8e70253a9159525b8d5e
Jallah <puncher@gmx.net>
2015-07-20 21:01:08 +0200

    implement a Nancy Test Action

--- src/Connect4Challenge.Web/Modules/GameModule.cs
+++ src/Connect4Challenge.Web/Modules/GameModule.cs
@@ -1,10 +1,13 @@
-﻿using Nancy;
+﻿using Connect4Challenge.Interface;
+using Nancy;
 using Nancy.Diagnostics.Modules;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Connect4Challenge.Web.Modules
 {
@@ -12,9 +15,12 @@ namespace Connect4Challenge.Web.Modules
     {
         public GameModule()
         {
-            Get["/"] = x =>
+            Get["/"] = _ =>
             {
-                return View["Index"];
+                var p = Path.Combine(Environment.CurrentDirectory, "..", "..","..", "..", "tests", "Connect4Challenge.TestImplementation", "bin", "Debug", "Connect4Challenge.TestImplementation.dll");
+                var o = Connect4Challenge.Bootstrapper.getSubClassFromAssembly<ConnectFour>(p);
+                var res = "Name: " + o.Name + " Move: " + o.Move(new int[7,6]);
+                return res; //View["Index",];
             };
         }
     }
